e028668efa8d28188c95cea8a5fa4b1af1b48c68
Vatani <shirinvatani97@gmail.com>
2023-10-05 00:54:55 +0330

    Animate commit

--- app/src/main/java/com/example/animatebottomnavigationinjetpack/MainActivity.kt
+++ app/src/main/java/com/example/animatebottomnavigationinjetpack/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.animatebottomnavigationinjetpack
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.animatebottomnavigationinjetpack.ui.theme.AnimateBottomNavigationInJetPackTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +27,50 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AnimateBottomNavigationInJetPackTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
+               // MainScreen()
             }
         }
     }
 }
 
+/*
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(){
+
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(bottom = 50.dp),
+    contentAlignment= Alignment.BottomCenter){
+
+        CustomBtnNavigation()
+    }
+}
+*/
+/*
+
+@Composable
+fun CustomBtnNavigation() {
+
+    Row(horizontalArrangement = Arrangement.SpaceBetween,
+    verticalAlignment = Alignment.CenterVertically,
+    modifier = Modifier
+        .height(80.dp)
+        .paint(
+            painter = painterResource(id = R.drawable.btn_nav),
+            contentScale = ContentScale.FillHeight
+        )){
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
+
 fun GreetingPreview() {
-    AnimateBottomNavigationInJetPackTheme {
-        Greeting("Android")
-    }
-}
+
+    MainScreen()
+
+}*/
